fe92e384c3038d23dbd6a234585aae22f344207e
yashaswinib019 <saiyashaswini.22cse@cambridge.edu.in>
2025-04-25 12:10:31 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,13 @@
+package cse.cambridge3.prg4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! Hi" );
+    }
+}
